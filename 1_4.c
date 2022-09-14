11da16ea7bd686586a4625faa0d4a70f839f2b19
Tunax-C <ariestina.tc@gmail.com>
2022-09-14 19:25:42 +0800

    2_1

---
+++ 1_4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+    int bottom;
+    int height;
+
+    printf("請輸入三角形的底(cm):");
+    scanf("%d",&bottom);
+    printf("請輸入三角形的高(cm):");
+    scanf("%d",&height);
+
+    printf("3角形的面積是%.2f\n", bottom * height / 2.0);
+    return 0;
+}
---
+++ 2_1.c
@@ -0,0 +1,14 @@
+//
+// Created by User on 14/09/2022.
+//
+
+#include <stdio.h>
+
+int main(void) {
+    int i;
+    printf("請輸入i的值:");
+    scanf("%d", &i);
+
+    printf("i的值為%d\n", i);
+    return 0;
+}
